3ef4b447efda16ad9701101473b539147a0f2d2b
Sourish <nit_sourish@rediffmail.com>
2019-09-19 19:48:51 +0200

    Delete functions.py

--- functions.py
+++
@@ -1,6 +0,0 @@
-def missing_quantity(data,column):
-    missing = pd.DataFrame(data[column].apply(lambda x: np.sum(x.isnull(), axis=0)))
-    missing['count_missing'] = missing.iloc[:,0]
-    missing['percentage_missing'] = (missing.iloc[:,0]/data.shape[0])*100
-    missing.drop(missing.columns[0],axis='columns',inplace=True)
-    return(missing)
